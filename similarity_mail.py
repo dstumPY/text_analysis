7cef07be194e7329aacbe1591117a680f173c3d6
dstumPY <dannystumpe@gmail.com>
2020-06-11 23:04:00 +0200

    Added similarity function

--- similarity_mail.py
+++ similarity_mail.py
@@ -2,14 +2,97 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-df = pd.read_csv('mail_data.csv')
-mails = df['emails']
+df = pd.read_csv("mail_data.csv")
+
+# preprocess mail strings
+df["emails_cleaned"] = df["emails"].str.replace(".", "")
+df["emails_cleaned"] = "_" + df["emails_cleaned"] + "_"
+
+mails = df["emails_cleaned"]
+
 
 def ngrams(string: str, n=3):
     ngrams = zip(*[string[i:] for i in range(n)])
-    return [''.join(ngram) for ngram in ngrams]
+    return ["".join(ngram) for ngram in ngrams]
 
 
 vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
 tf_idf_matrix = vectorizer.fit_transform(mails)
-print('bla')
+print("bla")
+
+import numpy as np
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct
+
+
+def awesome_cossim_top(A, B, ntop, lower_bound=0):
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+
+    idx_dtype = np.int32
+
+    nnz_max = M * ntop
+
+    indptr = np.zeros(M + 1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M,
+        N,
+        np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr,
+        indices,
+        data,
+    )
+
+    return csr_matrix((data, indices, indptr), shape=(M, N))
+
+
+def get_matches_df(sparse_matrix, name_vector, top=100):
+    non_zeros = sparse_matrix.nonzero()
+
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+
+    return pd.DataFrame(
+        {"left_side": left_side, "right_side": right_side, "similairity": similairity}
+    )
+
+
+import time
+
+t1 = time.time()
+matches = awesome_cossim_top(tf_idf_matrix, tf_idf_matrix.transpose(), 10, 0.8)
+t = time.time() - t1
+print("SELFTIMED:", t)
+
+
+matches_df = get_matches_df(matches, company_names, top=100000)
+matches_df = matches_df[matches_df["similairity"] < 0.99999]  # Remove all exact matches
+matches_df.sample(20)
